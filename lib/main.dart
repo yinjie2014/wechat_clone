be1be12072478ecd054fbd69bdc09b43dacbfe1f
jie <972547478@qq.com>
2019-05-08 16:26:00 +0800

    提交图片

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,18 @@ import 'package:flutter/cupertino.dart';
 import './home/home_screen.dart';
 import 'constants.dart' show AppColors;
 
-void main() => runApp(MaterialApp(
-  title: "微信",
-  home: HomeScreen(),
-  theme: ThemeData.light().copyWith(
-    primaryColor: Color(AppColors.AppBarColor),
-  )
-));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: "微信",
+        home: HomeScreen(),
+        theme: ThemeData.light().copyWith(
+          primaryColor: Color(AppColors.AppBarColor),
+        ));
+  }
+}
